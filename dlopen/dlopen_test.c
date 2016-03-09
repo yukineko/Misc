049b8f73efcda451a696118bd8b9e31a675bce39
yukinekonyan <yukinekonyan@gmail.com>
2016-03-09 15:36:23 +0900

    RTLD_GLOBALの追加

--- dlopen/dlopen_test.c
+++ dlopen/dlopen_test.c
@@ -3,12 +3,15 @@
 #include <stdlib.h>
 
 int main(int argc, char* argv[]){
+    // dlopenを確認したい
+    // dlopen("任意のlib****.so", RTLD_LAZY | ...);
+    
     char* error;
     if(argc < 2){
 	err(EXIT_FAILURE, "usage dlopen_test : filename");
 	return 1;
     }
-    dlopen(argv[1], RTLD_NOW);
+    dlopen(argv[1], RTLD_LAZY | RTLD_GLOBAL);
     error = dlerror();
     if(error != NULL){
 	err(EXIT_FAILURE, "dlopen error %s", error);
