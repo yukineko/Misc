3995564c2c478496c98efb9f7e6cff5517ebfc0c
nakayama <nakayama@logbar.j~p>
2018-04-23 20:23:08 +0900

    うごかないのをちょっとなおした

--- domclipper/js/background.js
+++ domclipper/js/background.js
@@ -2,7 +2,9 @@ chrome.extension.onMessage.addListener(function(req, sender, callback) {
   if (sender.tab) { // from a content script
     if (req.text !== undefined) {
 	var properties = { url: "./html/elements.html?text=" + req.text };
-      chrome.tabs.create(properties, function(tab) {});
+	chrome.tabs.create(properties, function(tab) {
+	    
+	});
     }
   }
 });
--- domclipper/js/content-script.js
+++ domclipper/js/content-script.js
@@ -1,6 +1,7 @@
 window.addEventListener('keydown', function(e){
     if(e.metaKey && e.keyCode == 13){
 	var selection = window.getSelection()
+	console.log(selection)
 	var nodestack = []
 	if(selection.rangeCount == 1){
 	    var range = selection.getRangeAt(0)
@@ -9,14 +10,13 @@ window.addEventListener('keydown', function(e){
 	    while((parent = parent.parentNode) != null){
 		nodestack.push()
 	    }
-//	    chrome.extension.sendMessage({ text: text.toString() }, function(response) {})
+	    text = selection.toString()
+	    console.log(text)
+	    chrome.extension.sendMessage({ text: text }, function(response) {})
 	} else if(selection.rangeCount > 1){
 	    var text = selection.getRangeAt(0).parentElement().innerHTML
 	    console.log(text)
 	    chrome.extension.sendMessage({ text: text }, function(response) {})	    
 	}
-
-
     }
-}, false)
-
+})
